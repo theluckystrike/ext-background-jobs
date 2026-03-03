faef27576eb5fc69d103cf9b8ccf940fcde3177a
theluckystrike <theluckystrike@zovo.one>
2026-03-04 01:16:48 +0700

    fix: remove node_modules from git, add gitignore

--- dist/lib.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Chrome Extension Library
- * Created by theluckystrike
- */
-export declare const library = "library";
-export declare const VERSION = "1.0.0";
--- dist/lib.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * Chrome Extension Library
- * Created by theluckystrike
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VERSION = exports.library = void 0;
-exports.library = "library";
-exports.VERSION = "1.0.0";
